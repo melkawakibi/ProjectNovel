98689aaf61796ecdcf97a25d4171b19093463ef6
SalElka <selkawakibi@gmail.com>
2016-09-27 20:57:33 +0200

    no message

--- projectnovel/resources/views/welcome.blade.php
+++ projectnovel/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Salahddin
                 </div>
 
                 <div class="links">
